ed6368c314dbe20abd836a5e2a40b072c88f998c
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-01-22 11:27:35 -0600

    Added the mod_id static variable If you need the mod id, do NationsAndKings.MOD_ID

--- src/main/java/com/nationsandkings/NationsAndKings.java
+++ src/main/java/com/nationsandkings/NationsAndKings.java
@@ -11,6 +11,9 @@ public class NationsAndKings implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("nationsandkings");
 
+	//just a string to get the mod id from instead of typing it out every time
+	public static final String MOD_ID = "nationsandkings";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
