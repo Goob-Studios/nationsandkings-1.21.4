4a8a087224ee292c36222b1c57df29fa4362abc9
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-01-28 00:18:28 -0600

    Villagers now pathfind to their bed at night. Will need to clear when the villager is sleeping.

--- src/main/java/com/nationsandkings/entity/ai/VillagerGenericSleep.java
+++ src/main/java/com/nationsandkings/entity/ai/VillagerGenericSleep.java
@@ -1,7 +1,13 @@
 package com.nationsandkings.entity.ai;
 
 import com.nationsandkings.entity.custom.GenericVillagerEntity;
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.pathing.Path;
+import net.minecraft.util.math.BlockPos;
+
 
 public class VillagerGenericSleep extends Goal {
     private final GenericVillagerEntity entity;
@@ -16,13 +22,26 @@ public class VillagerGenericSleep extends Goal {
         long time = entity.getWorld().getTimeOfDay(); // Get the total world time in ticks
         long timeOfDay = time % 24000;
 
-        return timeOfDay > 1300 || timeOfDay < 0;
+        return timeOfDay >= 13000;
     }
 
     @Override
     public boolean canStop() {
         long time = entity.getWorld().getTimeOfDay(); // Get the total world time in ticks
         long timeOfDay = time % 24000;
-        return timeOfDay <= 1300 && timeOfDay >= 1000;
+        return timeOfDay < 23000;
     }
+
+    @Override
+    public void start() {
+        if (entity.getHomeLocation() != null){
+            Path path = entity.getNavigation().findPathTo(entity.getHomeLocation(), 50);
+            if (path != null){
+                entity.getNavigation().startMovingAlong(path, 1.0);
+                System.out.println("Starting pathing");
+            }
+        }
+    }
+
+
 }
