3921c490c6749192077fb85ee9f9af08d9358ea1
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-12 15:41:42 -0600

    ~ Fixed all errors in GenericVillagerRenderer.java

--- src/client/java/com/nationsandkings/entity/GenericVillagerRenderer.java
+++ src/client/java/com/nationsandkings/entity/GenericVillagerRenderer.java
@@ -10,16 +10,17 @@ import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
-public class GenericVillagerRenderer extends MobEntityRenderer<GenericVillagerEntity, generic_villager_v2<GenericVillagerEntity>> {
+public class GenericVillagerRenderer extends MobEntityRenderer<GenericVillagerEntity,LivingEntityRenderState, generic_villager_v2<GenericVillagerEntity>> {
 
     public GenericVillagerRenderer(EntityRendererFactory.Context context) {
         super(context, new generic_villager_v2<>(context.getPart(ModModelLayers.GENERIC_VILLAGER)), 0.6f);
     }
 
     @Override
-    public EntityRenderState createRenderState() {
-        return null;
+    public LivingEntityRenderState createRenderState() {
+        return new LivingEntityRenderState();
     }
+
 
 //    @Override
 //    public Identifier getTexture(GenericVillagerEntity entity) {
@@ -27,15 +28,17 @@ public class GenericVillagerRenderer extends MobEntityRenderer<GenericVillagerEn
 //    }
 
     //Override is here to prepare for children in the future - for now it is unused.
-    @Override
+
     //Wants LivingEntityRenderState, matrixStack, vertexConsumerProvider, int
-    public void render(GenericVillagerEntity mobEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
-        super.render(mobEntity, matrixStack, vertexConsumerProvider, i);
+    public void render(LivingEntityRenderState renderState, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        //Mob entity needs to be a LivingEntityRenderState
+        super.render(renderState, matrixStack, vertexConsumerProvider, i);
     }
+
 
     @Override
     public Identifier getTexture(LivingEntityRenderState state) {
-        return Identifier.of(NationsAndKings.MOD_ID, "textures/entities/generic_villager/villager_guard.png");;
+        return Identifier.of(NationsAndKings.MOD_ID, "textures/entities/generic_villager/villager_guard.png");
     }
 
     //Need to put the get texture function here now
