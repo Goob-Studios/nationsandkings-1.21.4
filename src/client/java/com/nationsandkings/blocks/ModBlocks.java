23eff814e1c6717678eaaa50663ff8e12d18a35c
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-11 21:37:16 -0600

    + Added Mod Blocks This allows us to easily add items by calling this class

---
+++ src/client/java/com/nationsandkings/blocks/ModBlocks.java
@@ -0,0 +1,24 @@
+package com.nationsandkings.blocks;
+
+import com.nationsandkings.NationsAndKings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static Block register(Block block, String name, boolean shouldRegisterItem) {
+        Identifier id = new Identifier(NationsAndKings.MOD_ID, name);
+
+
+        // Only used if we want to register the item - I think most of the time we will but who knows
+        if (shouldRegisterItem) {
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, id, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+}
