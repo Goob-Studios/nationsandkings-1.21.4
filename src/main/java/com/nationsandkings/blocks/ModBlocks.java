7df6a7a845295f40a06bda34c91c1a0196a66828
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-13 18:28:06 -0600

    ~Modified ModBlocks.jav Put in the example register function from the blocks class that's built in - trying to see if I can figure out why our register function doesn't work.

--- src/main/java/com/nationsandkings/blocks/ModBlocks.java
+++ src/main/java/com/nationsandkings/blocks/ModBlocks.java
@@ -4,13 +4,20 @@ import com.nationsandkings.NationsAndKings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+
+//public static final Block TOWN_HALL;
 
 public class ModBlocks {
     public static Block register(Block block, String name, boolean shouldRegisterItem) {
@@ -27,13 +34,17 @@ public class ModBlocks {
 
         return Registry.register(Registries.BLOCK, id, block);
     }
-
     //Registering blocks here
     public static final Block TOWN_HALL = register(
             new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL)),
             "town_hall",
             true
     );
+    //Example from the built-in Blocks class
+//    public static Block register(RegistryKey<Block> key, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+//        Block block = (Block)factory.apply(settings.registryKey(key));
+//        return (Block)Registry.register(Registries.BLOCK, key, block);
+//    }
 
 
 
