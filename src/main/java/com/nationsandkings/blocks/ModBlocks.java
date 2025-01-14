596d6304ebed6be9498698bb3142672e1b8ad3cf
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-13 22:52:44 -0600

    ~Modified ModBlocks.java Removed some unused imports

--- src/main/java/com/nationsandkings/blocks/ModBlocks.java
+++ src/main/java/com/nationsandkings/blocks/ModBlocks.java
@@ -4,13 +4,11 @@ import com.nationsandkings.NationsAndKings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
@@ -21,17 +19,19 @@ import java.util.function.Function;
 
 public class ModBlocks {
 
+
+
     //Fabric Block Documentation: https://docs.fabricmc.net/develop/blocks/first-block
     // This is just for basic blocks, and not block entities. For now I'm implementing the work stations as a basic blocks
 
     //The block name and shouldRegisterItem are all null for some reason, despite them being passed in below.
-    public static Block register(Block block, String name, boolean shouldRegisterItem) {
+    private static Block register(Block block, String name, boolean shouldRegisterItem) {
         //They have a factory instead of an Identifier
         Identifier id = Identifier.of(NationsAndKings.MOD_ID, name);
 
 
         // Only used if we want to register the item - I think most of the time we will but who knows
-        //This is differnt than the blocks class but that's because I think they do it in the items class
+        //This is different than the blocks class but that's because I think they do it in the items class
         if (shouldRegisterItem) {
             BlockItem blockItem = new BlockItem(block, new Item.Settings());
             Registry.register(Registries.ITEM, id, blockItem);
@@ -42,9 +42,16 @@ public class ModBlocks {
         return Registry.register(Registries.BLOCK, id, block);
     }
     //Registering blocks here
+
     public static final Block TOWN_HALL = register(
             new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL)),
             "town_hall",
+            true
+    );
+
+    public static final Block BUTCHER_BLOCK = register(
+            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOD)),
+            "butcher_block",
             true
     );
     //Example from the built-in Blocks class
