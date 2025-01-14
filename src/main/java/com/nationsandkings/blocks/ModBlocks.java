4c765a87ce0518eb84019305cf626d671c1cf571
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-13 23:02:40 -0600

    ~Modified ModBlocks.java Removed more unused imports

--- src/main/java/com/nationsandkings/blocks/ModBlocks.java
+++ src/main/java/com/nationsandkings/blocks/ModBlocks.java
@@ -6,16 +6,13 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
-import java.util.function.Function;
-
-
-//public static final Block TOWN_HALL;
 
 public class ModBlocks {
 
