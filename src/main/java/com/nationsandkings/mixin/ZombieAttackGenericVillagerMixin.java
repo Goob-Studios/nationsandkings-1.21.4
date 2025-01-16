7fb889648b0c45b966e868e170a19b4101d4f31a
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-16 17:49:29 -0600

    ~Modified ZombieAttackGenericVillagerMixin.java Starting to design a mixin to add a goal so the genericVillager gets attacked

--- src/main/java/com/nationsandkings/mixin/ZombieAttackGenericVillagerMixin.java
+++ src/main/java/com/nationsandkings/mixin/ZombieAttackGenericVillagerMixin.java
@@ -1,4 +1,13 @@
 package com.nationsandkings.mixin;
 
+import net.minecraft.entity.mob.ZombieEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ZombieEntity.class)
 public class ZombieAttackGenericVillagerMixin {
+
+    private void init(CallbackInfo info) {
+        // This code is injected into the start of MinecraftServer.loadWorld()V
+    }
 }
