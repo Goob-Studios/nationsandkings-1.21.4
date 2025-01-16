d0c674ee6f02b410d034baf96216c308fe7e2b92
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-16 15:53:04 -0600

    +Added Register Function to ModItems.java

--- src/main/java/com/nationsandkings/items/ModItems.java
+++ src/main/java/com/nationsandkings/items/ModItems.java
@@ -1,8 +1,21 @@
 package com.nationsandkings.items;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+
 public class ModItems {
 
+    //From the fabric wiki, we can change later
+    public static Item register(Item item, RegistryKey<Item> registryKey) {
+        // Register the item.
+        Item registeredItem = Registry.register(Registries.ITEM, registryKey.getValue(), item);
 
+        // Return the registered item!
+        return registeredItem;
+
+    }
     public static void initialize() {
 
     }
