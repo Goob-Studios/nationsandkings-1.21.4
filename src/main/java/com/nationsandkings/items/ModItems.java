96e22b5f4818aa55d9e1716a35ae3bf7de529295
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-16 15:55:08 -0600

    +Added an untextured copper coin item to test the item groups

--- src/main/java/com/nationsandkings/items/ModItems.java
+++ src/main/java/com/nationsandkings/items/ModItems.java
@@ -1,9 +1,13 @@
 package com.nationsandkings.items;
 
+import com.nationsandkings.NationsAndKings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
 
 public class ModItems {
 
@@ -16,6 +20,15 @@ public class ModItems {
         return registeredItem;
 
     }
+
+    // Start registering items here
+
+    public static final RegistryKey<Item> COPPER_COIN_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NationsAndKings.MOD_ID, "copper_coin"));
+    public static final Item SUSPICIOUS_SUBSTANCE = register(
+            new Item(new Item.Settings().registryKey(COPPER_COIN_KEY)),
+            COPPER_COIN_KEY
+    );
+
     public static void initialize() {
 
     }
