f9a6c79ddd236073f9e2c151886c299ec63f5dfc
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-16 17:54:01 -0600

    +Created AccessZombieMixin.java to access GoalSelector ~ Modified ZombieAttackGenericVillagerMixin.java to have most of the code once the Accessor Mixin is working

--- src/main/java/com/nationsandkings/mixin/ZombieAttackGenericVillagerMixin.java
+++ src/main/java/com/nationsandkings/mixin/ZombieAttackGenericVillagerMixin.java
@@ -1,13 +1,21 @@
 package com.nationsandkings.mixin;
 
+import com.nationsandkings.entity.custom.GenericVillagerEntity;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ZombieEntity.class)
 public class ZombieAttackGenericVillagerMixin {
-
+    @Inject(at = @At("HEAD"), method = "initCustomGoals")
     private void init(CallbackInfo info) {
-        // This code is injected into the start of MinecraftServer.loadWorld()V
+        ZombieEntity zombie = (ZombieEntity) (Object) this;
+        GoalSelector goalSelector = ((ZombieEntityAccessor) zombie).getGoalSelector();
+        goalSelector.add(2, new ActiveTargetGoal(zombie, GenericVillagerEntity.class, true));
     }
 }
