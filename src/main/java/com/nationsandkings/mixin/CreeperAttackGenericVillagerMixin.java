bf3db23fabb02a120f96e55b502c38f40d0c0563
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-01-16 19:32:02 -0600

    ~Modified CreeperAttackGenericVillagerMixin.java trying to fix the Creeper not attacking

--- src/main/java/com/nationsandkings/mixin/CreeperAttackGenericVillagerMixin.java
+++ src/main/java/com/nationsandkings/mixin/CreeperAttackGenericVillagerMixin.java
@@ -4,7 +4,6 @@ import com.nationsandkings.entity.custom.GenericVillagerEntity;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.mob.ZombieEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
