3b8a67691ec2fc1d2f7f9e9544eccd2b6fbd5903
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-02-04 16:03:21 -0600

    ~ Modified VillagerDefendTask.java  Copied a lot of the MeleeAttackTask single we'll have to extend this one anyways

--- src/main/java/com/nationsandkings/entity/ai/tasks/VillagerDefendTask.java
+++ src/main/java/com/nationsandkings/entity/ai/tasks/VillagerDefendTask.java
@@ -1,14 +1,50 @@
 package com.nationsandkings.entity.ai.tasks;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.EntityLookTarget;
+import net.minecraft.entity.ai.brain.LivingTargetCache;
 import net.minecraft.entity.ai.brain.MemoryModuleState;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.MultiTickTask;
+import net.minecraft.entity.ai.brain.task.SingleTickTask;
+import net.minecraft.entity.ai.brain.task.TaskTriggerer;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.util.Hand;
 
 import java.util.Map;
+import java.util.function.Predicate;
 
-public class VillagerDefendTask <E extends PathAwareEntity> extends MultiTickTask<E> {
-    public VillagerDefendTask(Map<MemoryModuleType<?>, MemoryModuleState> requiredMemoryState) {
-        super(requiredMemoryState);
+public class VillagerDefendTask{
+
+    public VillagerDefendTask(){
+
     }
+
+
+    public static <T extends MobEntity> SingleTickTask<T> create(int cooldown) {
+        return create((target) -> {
+            return true;
+        }, cooldown);
+    }
+
+    public static <T extends MobEntity> SingleTickTask<T> create(Predicate<T> targetPredicate, int cooldown) {
+        return TaskTriggerer.task((context) -> {
+            return context.group(context.queryMemoryOptional(MemoryModuleType.LOOK_TARGET), context.queryMemoryValue(MemoryModuleType.ATTACK_TARGET), context.queryMemoryAbsent(MemoryModuleType.ATTACK_COOLING_DOWN), context.queryMemoryValue(MemoryModuleType.VISIBLE_MOBS)).apply(context, (lookTarget, attackTarget, attackCoolingDown, visibleMobs) -> {
+                return (world, entity, time) -> {
+                    LivingEntity livingEntity = (LivingEntity)context.getValue(attackTarget);
+                    if (targetPredicate.test(entity) && entity.isInAttackRange(livingEntity) && ((LivingTargetCache)context.getValue(visibleMobs)).contains(livingEntity)) {
+                        lookTarget.remember(new EntityLookTarget(livingEntity, true));
+                        entity.swingHand(Hand.MAIN_HAND);
+                        entity.tryAttack(world, livingEntity);
+                        attackCoolingDown.remember(true, (long)cooldown);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+            });
+        });
+    }
+
 }
