efbacc664e32b1b8c89b024b91e0141a0bbfe87e
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-02-04 15:34:09 -0600

    ~ Modified VillagerDefendTask.java  Added the constructor call to the super class (task)

--- src/main/java/com/nationsandkings/entity/ai/tasks/VillagerDefendTask.java
+++ src/main/java/com/nationsandkings/entity/ai/tasks/VillagerDefendTask.java
@@ -1,4 +1,14 @@
 package com.nationsandkings.entity.ai.tasks;
 
-public class VillagerDefendTask {
+import net.minecraft.entity.ai.brain.MemoryModuleState;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.ai.brain.task.MultiTickTask;
+import net.minecraft.entity.mob.PathAwareEntity;
+
+import java.util.Map;
+
+public class VillagerDefendTask <E extends PathAwareEntity> extends MultiTickTask<E> {
+    public VillagerDefendTask(Map<MemoryModuleType<?>, MemoryModuleState> requiredMemoryState) {
+        super(requiredMemoryState);
+    }
 }
