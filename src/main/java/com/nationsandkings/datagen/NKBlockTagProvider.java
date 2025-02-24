fbb9db4f4e092e9bea0c8e14934d972bf944d55b
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-02-23 20:44:34 -0600

    ~ Modified NKBlockTagProvider.java ~ Modified NKItemTagProvider.java + Added NKModelProvider.java - Removed NKDataGenerator.java Trying to rework data generation. Hope to add spawn eggs as well as some other stuff.

---
+++ src/main/java/com/nationsandkings/datagen/NKBlockTagProvider.java
@@ -0,0 +1,21 @@
+package com.nationsandkings.datagen;
+
+import com.nationsandkings.blocks.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class NKBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public NKBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.TOWN_HALL);
+    }
+}
--- src/main/java/com/nationsandkings/datagen/NKModelProvider.java
+++ src/main/java/com/nationsandkings/datagen/NKModelProvider.java
@@ -1,28 +1,4 @@
 package com.nationsandkings.datagen;
 
-import com.nationsandkings.items.ModItems;
-import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.client.data.Model;
-import net.minecraft.util.Identifier;
-
-import java.util.Optional;
-
-public class NKModelProvider extends FabricModelProvider {
-    public NKModelProvider(FabricDataOutput output) {
-        super(output);
-    }
-
-    @Override
-    public void generateBlockStateModels(net.minecraft.client.data.BlockStateModelGenerator blockStateModelGenerator) {
-
-    }
-
-    @Override
-    public void generateItemModels(net.minecraft.client.data.ItemModelGenerator itemModelGenerator) {
-
-        itemModelGenerator.register(ModItems.GENERIC_VILLAGER_SPAWN_EGG,
-                new Model(Optional.of(Identifier.of("minecraft", "item/template_spawn_egg")), Optional.empty()));
-
-    }
+public class NKModelProvider {
 }
