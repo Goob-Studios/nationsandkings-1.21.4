251a78e5460d2889d3a16ce0850d941760487fbd
Spencer Blackwell <spencerblackwell02@gmail.com>
2025-03-02 17:44:02 -0600

    ~Modified GenericVillagerEntity.java Removed some of the logger statements ~ ModBlocks.java Removed some of the unused comments

--- src/main/java/com/nationsandkings/blocks/ModBlocks.java
+++ src/main/java/com/nationsandkings/blocks/ModBlocks.java
@@ -31,15 +31,6 @@ public class ModBlocks {
     //The block name and shouldRegisterItem are all null for some reason, despite them being passed in below.
     //String name, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings, boolean shouldRegisterItem
     private static Block register(Block block, RegistryKey<Block> blockKey, boolean shouldRegisterItem) {
-//        NationsAndKings.LOGGER.info("BLOCK: " + block);
-//        NationsAndKings.LOGGER.info("Name: " + name);
-//        NationsAndKings.LOGGER.info("ShouldRegisterItem: " + shouldRegisterItem);
-//        //They have a factory instead of an Identifier
-//        Identifier id = Identifier.of(NationsAndKings.MOD_ID, name);
-
-
-        // Only used if we want to register the item - I think most of the time we will but who knows
-        //This is different than the blocks class but that's because I think they do it in the items class
         if (shouldRegisterItem) {
             RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, blockKey.getValue());
 
@@ -47,9 +38,6 @@ public class ModBlocks {
             Registry.register(Registries.ITEM, itemKey, blockItem);
 
         }
-        //This is the same as the built-in class
-//        final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(NationsAndKings.MOD_ID, name));
-//        return Blocks.register(registryKey, factory, settings);
         return Registry.register(Registries.BLOCK, blockKey, block);
     }
 
@@ -69,35 +57,9 @@ public class ModBlocks {
             true
     );
 
-//    public static final Block TOWN_HALL = register(
-//            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS)),
-//            "town_hall",
-//            true
-//    );
-
-//    public static final Block TOWN_HALL = register("town_hall", Block::new, AbstractBlock.Settings.create().sounds(BlockSoundGroup.METAL), true);
-
-//    public static final Block BUTCHER_BLOCK = register("butcher_block", Block::new, AbstractBlock.Settings.create().sounds(BlockSoundGroup.WOOD), true);
-
-//    public static final Block BUTCHER_BLOCK = register(
-//            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS)),
-//            "butcher_block",
-//            true
-//    );
-    //Example from the built-in Blocks class
-//    public static Block register(RegistryKey<Block> key, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
-//        Block block = (Block)factory.apply(settings.registryKey(key));
-//        return (Block)Registry.register(Registries.BLOCK, key, block);
-//    }
-
 
 
     public static void initialize() {
-
-//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
-//            entries.add(ModBlocks.TOWN_HALL.asItem());
-//            entries.add(ModBlocks.BUTCHER_BLOCK);
-//        });
 
 
         ItemGroupEvents.modifyEntriesEvent(ModItems.WORKSTATIONS_GROUP_KEY).register((itemGroup) -> {
